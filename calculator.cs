c2dd4af0ed59cee778551681bcc97a7924b64fae
Jackie Snead <jackiesneadthe3rd@outlook.com>
2021-03-29 22:26:23 -0500

    Add files via upload

---
+++ calculator.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Decimal a, b, c;
+            string input;
+
+
+            Console.Write("Enter the length of the room:");
+            input = Console.ReadLine();
+            a = Decimal.Parse(input);
+
+            Console.Write("Enter the width of the room:");
+            input = Console.ReadLine();
+            b = Decimal.Parse(input);
+
+            Console.Write("Enter the overage percent:");
+            input = Console.ReadLine();
+            c = Decimal.Parse(input);
+
+            Decimal totalarea = (a * b + (c/100 * a * b));
+
+            Console.Write("The total area, including overage, is: " + totalarea);
+        }
+    }
+}
+
